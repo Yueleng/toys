7d8504bd6dfb3602c7eede61e0452b48b7ac9c7c
Alan <wangyueleng@gmail.com>
2025-12-26 19:47:59 -0500

    Add middleware to protect /user-info route
    Introduces middleware that checks authentication for protected routes, redirecting unauthenticated users to the home page. Currently, only the /user-info route is protected.

---
+++ next-auth/app/middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { auth } from "@/app/auth";
+
+const protectedRoutes = ["/user-info"];
+
+export async function middleware(req: NextRequest) {
+  const session = await auth();
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    req.nextUrl.pathname.startsWith(route)
+  );
+
+  if (isProtectedRoute && !session) {
+    // return NextResponse.redirect(new URL("/api/auth/signin", req.url));
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+}
